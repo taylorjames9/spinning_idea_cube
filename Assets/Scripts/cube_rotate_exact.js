719151c19944daef26bc5b2265c25deb01b7cc76
James Taylor <taylorjames9@gmail.com>
2013-04-15 15:07:44 -0700

    trying out a new script for spinning cube - this one turns by 90 degrees

---
+++ Assets/Scripts/cube_rotate_exact.js
@@ -0,0 +1,35 @@
+
+var rotating : boolean = false;
+var rotateSpeed : int = 45;
+
+ 
+
+function OnMouseDown()
+
+{
+
+   if (!rotating) {
+
+      rotating = true;
+
+      var curRot : float = 0;
+
+      var startRot : float = transform.eulerAngles.y;
+
+      while (curRot < 90) {
+
+         curRot += rotateSpeed * Time.deltaTime;
+
+         transform.eulerAngles.y = startRot + curRot;
+
+         yield;
+
+      }
+
+      transform.eulerAngles.y = Mathf.Round(startRot + 90);
+
+      rotating = false;
+
+   }
+
+}
