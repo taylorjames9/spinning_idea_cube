eb446f3bf1659dd901ae12e0b13079a5d25d16a2
James Taylor <taylorjames9@gmail.com>
2013-04-15 19:26:16 -0700

    six capsules in place. Working on snapping cube into place.

--- Assets/Scripts/spin_cube_mouseDrag.js
+++ Assets/Scripts/spin_cube_mouseDrag.js
@@ -1,5 +1,16 @@
 
+var neg_x : Transform;
+var plus_x : Transform;
+var neg_y : Transform;
+var plus_y : Transform;
+var neg_z : Transform;
+var plus_z : Transform;
 
+var snapSpeed = 1.0;
+
+
+var qrotation : Quaternion;
+var qRotationFrom : Quaternion;
 
 var rotationSpeed = 1.0;
 var lerpSpeed = 1.0;
@@ -32,4 +43,63 @@ function Update ()
     transform.Rotate( Camera.main.transform.up * speed.x * rotationSpeed, Space.World );
     transform.Rotate( Camera.main.transform.right * speed.y * rotationSpeed, Space.World );
  
+SnapTo();
+
 }
+
+
+function SnapTo()
+{
+	if(dragging == false){
+ 		if(transform.rotation.y < 45 && transform.rotation.y > 315 && transform.rotation.z < 45 && transform.rotation.z > 315){
+ 			qrotation = Quaternion.LookRotation(plus_x.position);
+ 			qRotationFrom = Quaternion.LookRotation(transform.position);
+ 			 transform.rotation = Quaternion.Lerp(qRotationFrom, qrotation, Time.deltaTime * snapSpeed);
+ 			 print("We should be snapping to plus_x object");
+ 
+        }
+        if(transform.rotation.y < 225  && transform.rotation.y > 135 && transform.rotation.z < 225 && transform.rotation.z > 135){
+ 			qrotation = Quaternion.LookRotation(neg_x.position);
+ 			qRotationFrom = Quaternion.LookRotation(transform.position);
+ 			transform.rotation = Quaternion.Lerp(qRotationFrom, qrotation, Time.deltaTime * snapSpeed);
+        	 print("We should be snapping to neg_x object");
+        }
+}
+}
+
+/*
+	if(dragging = false){
+ 		if(transform.rotation.x < 45 && transform.rotation.x > 315 && transform.rotation.y < 45 && transform.rotation.y > 315 && transform.rotation.z < 45 && transform.rotation.z > 315){
+ 			var qrotation = Quaternion.LookRotation(plus_x.position);
+ 			 ThisTransform.rotation = Quaternion.Lerp(ThisTransform.rotation, qrotation, Time.deltaTime * speed);
+ 
+        }
+        if(transform.rotation.x < NUM && transform.rotation.x > NUM && transform.rotation.y < 225  && transform.rotation.y > 135 && transform.rotation.z < NUM && transform.rotation.z > NUM){
+ 			var qrotation = Quaternion.LookRotation(neg_x.position);
+ 			 ThisTransform.rotation = Quaternion.Lerp(ThisTransform.rotation, qrotation, Time.deltaTime * speed);
+ 
+        }
+        
+        
+        if(transform.rotation.x < NUM && transform.rotation.x > NUM && transform.rotation.y < NUM && transform.rotation.y > NUM && transform.rotation.z < NUM && transform.rotation.z > NUM){
+ 			var qrotation = Quaternion.LookRotation(neg_y.position);
+ 			 ThisTransform.rotation = Quaternion.Lerp(ThisTransform.rotation, qrotation, Time.deltaTime * speed);
+ 
+        }
+        if(transform.rotation.x < NUM && transform.rotation.x > NUM && transform.rotation.y < NUM && transform.rotation.y > NUM && transform.rotation.z < NUM && transform.rotation.z > NUM){
+ 			var qrotation = Quaternion.LookRotation(plus_y.position);
+ 			 ThisTransform.rotation = Quaternion.Lerp(ThisTransform.rotation, qrotation, Time.deltaTime * speed);
+ 
+        }
+        if(transform.rotation.x < NUM && transform.rotation.x > NUM && transform.rotation.y < NUM && transform.rotation.y > NUM && transform.rotation.z < NUM && transform.rotation.z > NUM){
+ 			var qrotation = Quaternion.LookRotation(neg_z.position);
+ 			 ThisTransform.rotation = Quaternion.Lerp(ThisTransform.rotation, qrotation, Time.deltaTime * speed);
+ 
+        }
+        if(transform.rotation.x < NUM && transform.rotation.x > NUM && transform.rotation.y < NUM && transform.rotation.y > NUM && transform.rotation.z < NUM && transform.rotation.z > NUM){
+ 			var qrotation = Quaternion.LookRotation(plus_z.position);
+ 			 ThisTransform.rotation = Quaternion.Lerp(ThisTransform.rotation, qrotation, Time.deltaTime * speed);
+ 
+        }
+    }
+}*/
