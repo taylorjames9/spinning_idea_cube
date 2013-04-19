16a39fab621dd26da06a8ae66298e7f22d35d6f5
James Taylor <taylorjames9@gmail.com>
2013-04-19 09:52:16 -0700

    has gui buttons but this cube still rotates slowly, painstakingly.

---
+++ Assets/Scripts/rotatePlane.js
@@ -0,0 +1,24 @@
+	
+	
+var cubeScript : dots_on_cube; 
+var guiScript: Gui;
+	
+	
+function Update(){
+	
+	if(!cubeScript.dragging && guiScript.tellCubeRotate == true){
+
+
+
+var lowestIndexforSwitch0 = cubeScript.lowestIndex;
+
+  if(lowestIndexforSwitch0 == 0){
+    	
+        transform.rotation *= Quaternion.Euler(-90, 0, 0);
+		print("Cube should have just rotated 90 Degrees counterclockwise");
+		guiScript.tellCubeRotate = false;
+    				
+    }
+    		
+}
+}
---
+++ Assets/Scripts/rotatePlane_ceo.js
@@ -0,0 +1,20 @@
+var cubeScript : dots_on_cube; 
+var guiScript: Gui;
+	
+	
+function Update(){
+	
+	if(!cubeScript.dragging && guiScript.tellCubeRotate == true){
+
+var lowestIndexforSwitch0 = cubeScript.lowestIndex;
+
+  if(lowestIndexforSwitch0 == 0){
+    	
+        transform.rotation *= Quaternion.Euler(0, -90, 0);
+		print("Cube should have just rotated 90 Degrees counterclockwise");
+		guiScript.tellCubeRotate = false;
+    				
+    }
+    		
+}
+}
